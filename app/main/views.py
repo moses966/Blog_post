39cd3eea14364c603910aa111a0867d8f5e4edd0
Muwawu Moses <mosesmuwawu@gmail.com>
2023-05-08 18:03:45 +0300

    first commit

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from . import main
+from .forms import NameForm
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
